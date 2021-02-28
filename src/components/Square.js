5e46582942a2d0126d8c08471258b40c10df7ad9
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-28 15:32:55 -0500

    x image displaying on square, but smooshing text to the side

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Square = (props) => {
-  const handleClick = () => {
 
+  const [toggled, setToggled] = React.useState(false);
+
+  const toggleClick = () => {
+    console.log("i've been clicked")
+    setToggled(!toggled)
   }
 
   return (
-    <div className={props.cssClass}>
+    <div className={props.cssClass} onClick={toggleClick}>
+      {toggled && <img src="https://raw.githubusercontent.com/ronniekram/bingo/main/src/bingo-x.png" alt="Bingo X" className="bingo-x"/>}
       {props.text}
       <span className="bingo-card__checkbox">
         {/* {props.text} */}
