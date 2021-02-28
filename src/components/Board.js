82069b7e76fcc74861904b750924edfe1fd6712b
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-27 22:16:11 -0500

    created random array of square text

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import _ from "lodash";
 import Square from './Square';
+import squares from '../list';
 
 const Board = () => {
-
+  let gameSquares = _.sampleSize(squares, 24)
+  gameSquares.splice(13, 0, "FREE")
   return (
     <div className="main-content">
       <div className="title">
         <span> // AAQ BINGO // </span>
       </div>
       <div className="bingo-card">
+        {console.log(gameSquares)}
         <Square />
         <Square />
         <Square />
