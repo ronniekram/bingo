11777217ba3fac65753b3004edb587f3729c3637
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-28 12:54:25 -0500

    commented out all media css, reformatting

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Square = (props) => {
   return (
-    <div className="bingo-card-item">
+    <div className="bingo-card__item">
       {props.text}
-      <span className="bingo-card-checkbox">
+      <span className="bingo-card__checkbox">
         {props.text}
       </span>
   </div>
